fb35696e7648afd47a079d03f244e723450e7ae4
GABO <gabrielvazquezcivica@gmail.com>
2026-09-24 13:28:37 -0600

    Crear config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const config = {
+    BOT_NAME: 'EXCLUSIVE BOT',
+    OWNER_NAME: 'EXCLUSIVE',
+
+    // Números de dueños
+    owner: [
+        '18145076729',
+        ''
+    ],
+
+    // IDs / LID de dueños
+    ownerLid: [
+        '279302511845418',
+        ''
+    ],
+
+    PREFIX: '.'
+}
+
+export default config
